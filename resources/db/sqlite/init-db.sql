1e85a2d7e55d3c2c25e18428560a874f500dc479
George Peristerakis <peristeri@users.noreply.github.com>
2022-02-21 22:47:25 +0200

    Modified the research table to match the README description

--- resources/db/sqlite/init-db.sql
+++ resources/db/sqlite/init-db.sql
@@ -1,15 +1,21 @@
-create table account (
+create table if not exists account (
   id text not null primary key,
   username text unique not null,
   password text not null,
   role text not null,
+  created_at datetime default current_timestamp,
+  is_active boolean default true,
   check (role in ("researcher", "worker"))
 );
 
-create table research (
+create table if not exists research (
   id text not null primary key,
   leader text not null,
   title text not null,
-  hypothesis text not null,
+  specification text not null,
+  data_repository text not null,
+  version text not null,
+  created_at datetime default current_timestamp,
+  is_active boolean default true,
   foreign key(leader) references account(id)
 );
