82b4f13383b80f457de7e222cca4e95bc4038d50
George Peristerakis <peristeri@users.noreply.github.com>
2022-03-06 17:49:35 +0200

    Changed the naming of the different initialization components

--- src/pyyp/config.clj
+++ src/pyyp/config.clj
@@ -7,15 +7,19 @@
             ))
 
 (def config {:database/connection "jdbc:sqlite:resources/db.sqlite"
-             :backend/application {:db             (ig/ref :database/connection)
-                                   :jwt-secret     "ff363172-d6b5-46f4-889e-858dbf200d43"
-                                   :worker-channel (ig/ref :channel/worker)}
-             :backend/server      {:application (ig/ref :backend/application)
-                                   :port        3000
-                                   :join?       false}
-             :channel/worker      {:size 64}
-             :backend/worker      {:channel (ig/ref :channel/worker)
-                                   :db      (ig/ref :database/connection)}})
+             :backend/application
+             {:db                  (ig/ref :database/connection)
+              :jwt-secret          "ff363172-d6b5-46f4-889e-858dbf200d43"
+              :build-research-chan (ig/ref :build-research/channel)}
+             :backend/server
+             {:application (ig/ref :backend/application)
+              :port        3000
+              :join?       false}
+             :build-research/channel
+             {:size 64}
+             :build-research/worker
+             {:channel (ig/ref :build-research/channel)
+              :db      (ig/ref :database/connection)}})
 
 (defmethod ig/init-key :database/connection [_ db-connection]
   (jdbc/get-connection db-connection))
@@ -29,15 +33,14 @@
 (defmethod ig/halt-key! :backend/server [_ server]
   (.stop server))
 
-(defmethod ig/init-key :channel/worker [_ {:keys [size]}]
+(defmethod ig/init-key :build-research/channel [_ {:keys [size]}]
   (async/chan (async/buffer size)))
 
-(defmethod ig/halt-key! :channel/worker [_ channel]
+(defmethod ig/halt-key! :build-research/channel [_ channel]
   (async/close! channel))
 
-(defmethod ig/init-key :backend/worker [_ {:keys [channel db]}]
-  (defn placeholder-worker [channel db]
-    (async/go-loop []
-      (when-let [message (async/<! channel)]
-        (println "Message received: " message)
-        (recur)))))
+(defmethod ig/init-key :build-research/worker [_ {:keys [channel]}]
+  (async/go-loop []
+    (when-let [message (async/<! channel)]
+      (println "Message received: " message)
+      (recur))))
