5fb0d573395aa6a9aea0e0e09e79c13ac2c806f6
George Peristerakis <peristeri@users.noreply.github.com>
2022-05-06 09:12:29 +0300

    Update the project aliases in the emacs config

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((cider-clojure-cli-aliases . ":dev:test:lib/sayid:repl/cider-refactor")
+         (cider-default-cljs-repl . shadow)
+         (cider-offer-to-open-cljs-app-in-browser . nil)))
+ )
